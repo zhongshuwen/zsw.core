381bb9b1c6ae499b15e4944290ca927a128de6e0
cf <carter@carter.at>
2022-01-22 22:31:54 +0800

    test perms

--- contracts/zsw.perms/src/zsw.perms.cpp
+++ contracts/zsw.perms/src/zsw.perms.cpp
@@ -8,20 +8,25 @@ ACTION zswperms::setperms(eosio::name scope, eosio::name user, uint128_t perm_bi
     auto tbl_permissions_scope = get_tbl_permissions(scope);
     eosio::print("test 2");
     auto itr = tbl_permissions_scope.find(user.value);
+    eosio::print("test 3");
 
     eosio::name ram_payer = scope;
+    eosio::print("test 4");
 
     if( itr == tbl_permissions_scope.end() ) {
+    eosio::print("test 5");
         tbl_permissions_scope.emplace(ram_payer, [&]( auto& row ) {
             row.user = user;
             row.perm_bits = perm_bits;
         });
     } else {
+    eosio::print("test 6");
         tbl_permissions_scope.modify(itr, ram_payer, [&]( auto& row ) {
             row.user = user;
             row.perm_bits = perm_bits;
         });
     }
+    eosio::print("test 7");
 }
 
 zswperms::t_permissions zswperms::get_tbl_permissions(name acc) {
