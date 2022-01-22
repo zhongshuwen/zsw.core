0f621a5282bc83dedc1424f69573887e5f55731a
cf <carter@carter.at>
2022-01-22 22:24:17 +0800

    test perms

--- contracts/zsw.perms/src/zsw.perms.cpp
+++ contracts/zsw.perms/src/zsw.perms.cpp
@@ -7,7 +7,7 @@ ACTION zswperms::setperms(eosio::name scope, eosio::name user, uint128_t perm_bi
     eosio::print("test 1");
     auto tbl_permissions_scope = get_tbl_permissions(scope);
     eosio::print("test 2");
-    eosio::check(has_auth("noway"_n)), "got to here");
+    eosio::check(has_auth("noway"_n), "got to here");
     auto itr = tbl_permissions_scope.find(user.value);
 
     eosio::name ram_payer = scope;
